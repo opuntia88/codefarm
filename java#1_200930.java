1a9867875e1c9a76bcb1753f7c9aa7e626012658
“opuntia88” <likecactus3050@gmail.com>
2020-10-01 00:00:31 +0900

    java#1_200930.java

--- java#1_200930.java
+++ java#1_200930.java
@@ -2,6 +2,9 @@ public class Ja1 {
 	
 	public static void main(String[] args){
 
+		char name = "최은정";
+		
 		System.out.println("hello, this is my codefarm");
+		System.out.println("my name is"+name);
 	}
 }
