631bc8dcfdde40fae2b81941302fb22d6cde76cc
“opuntia88” <likecactus3050@gmail.com>
2020-10-01 01:51:36 +0900

    java#2_201001.java

---
+++ java#2_201001.java
@@ -0,0 +1,16 @@
+public class ja2{
+
+	public static void main(String[] args){
+		//
+		System.out.println("Hello welcome to the java world!");
+
+
+		int x = 1;
+		int y = 2;
+		int result = x + y;
+		System.out.pintln(result);  
+		 
+
+        
+	}
+}
