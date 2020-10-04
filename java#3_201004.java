ef8dd343bb17be314eca057a8c2c6d52f7deb634
“opuntia88” <likecactus3050@gmail.com>
2020-10-04 23:06:11 +0900

    java#3_201004.java

---
+++ java#3_201004.java
@@ -0,0 +1,16 @@
+public class java3{
+	public static void main(String[] args)
+
+	byte var1=-128;
+	byte var2=-20;
+	byte var3=0;
+	byte var4=39;
+	byte var5=128;
+
+	System.out.println(var1);
+	System.out.println(var2);
+	System.out.println(var3);
+	System.out.println(var4);
+	System.out.println(var5);
+
+}
