4d841ea5bd43ec685d9596d39dc6d9d61b9b11ff
“opuntia88” <likecactus3050@gmail.com>
2020-10-04 23:02:27 +0900

    java#2_201001.java

--- java#2_201001.java
+++ java#2_201001.java
@@ -5,12 +5,12 @@ public class ja2{
 		System.out.println("Hello welcome to the java world!");
 
 
-		int x = 1;
+		int x = 1; //해당 변수는 1로 초기화 됨과동시에 선언 및 값 지정이 됨.(초기화 되지않으면 오류가 남; 컴파일에러)
 		int y = 2;
 		int result = x + y;
 		System.out.pintln(x+"+"+y+"="+result);  
 
-
+		
         
 	}
 }
