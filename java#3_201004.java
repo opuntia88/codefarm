0b785679e5252d8970b79ee06d38b81b33a76528
“opuntia88” <likecactus3050@gmail.com>
2020-10-04 23:43:33 +0900

    java#3_201004.java

--- java#3_201004.java
+++ java#3_201004.java
@@ -5,12 +5,23 @@ public class java3{
 	byte var2=-20;
 	byte var3=0;
 	byte var4=39;
-	byte var5=128;
+	//byte var5=128;
+	//byte는 -128~127사이, 즉 256개만을 다룸
 
 	System.out.println(var1);
 	System.out.println(var2);
 	System.out.println(var3);
 	System.out.println(var4);
-	System.out.println(var5);
+	//System.out.println(var5);
+
+	byte var6 = 125;
+	int var7 = 125;
+
+	for(int i=0;i<5;i++){
+		var6++;
+		var7++;
+		System.out.println("var6: "+var6+"\t"+"var7"+var7);
+		//\t는 tab칸만큼 칸을 띄워주는 역할을 한다.
+	}
 
 }
