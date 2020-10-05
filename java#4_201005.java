3ddd935e351eda37fe7a49a93cacd32060dd9543
“opuntia88” <likecactus3050@gmail.com>
2020-10-05 23:59:13 +0900

    java#4_201005.java

---
+++ java#4_201005.java
@@ -0,0 +1,13 @@
+public class java4{
+	public static void main(String[] args){
+
+		int var1 = 10; //10진수
+		int var2 = 012; //8진수
+		int var3 = 0XA; //16진수
+
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+
+	}
+}
