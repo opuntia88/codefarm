9b036e6f180c38ae56ffb03c5da340cf12465f53
“opuntia88” <likecactus3050@gmail.com>
2020-10-10 21:40:15 +0900

    java#5_201010.java

---
+++ java#5_201010.java
@@ -0,0 +1,17 @@
+public class java5{
+	public static void public static void main(String[] args) {
+		
+		long var1 = 10;
+		long var2 = 20L;
+		//long var3 = 1000000000;
+		long var4 = 1000000000000L;
+
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+
+		
+
+
+	}
+}
